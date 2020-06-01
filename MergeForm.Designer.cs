865642a89f409e8c0f7f917609452f9fc7f877d7
Suvidriel <suvidriel@gmail.com>
2020-06-01 19:17:19 +0300

    beta
    The first version

---
+++ MergeForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace VRoidHairMerger
+{
+    partial class MergeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourcePreset = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.destPreset = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sourcePreset
+            // 
+            this.sourcePreset.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sourcePreset.FormattingEnabled = true;
+            this.sourcePreset.Location = new System.Drawing.Point(15, 25);
+            this.sourcePreset.Name = "sourcePreset";
+            this.sourcePreset.Size = new System.Drawing.Size(197, 21);
+            this.sourcePreset.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source preset";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination preset";
+            // 
+            // destPreset
+            // 
+            this.destPreset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.destPreset.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.destPreset.FormattingEnabled = true;
+            this.destPreset.Location = new System.Drawing.Point(266, 25);
+            this.destPreset.Name = "destPreset";
+            this.destPreset.Size = new System.Drawing.Size(197, 21);
+            this.destPreset.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(358, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Merge";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MergeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 115);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.destPreset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sourcePreset);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MergeForm";
+            this.Text = "VRoidHairMerger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox sourcePreset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox destPreset;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MergeForm.cs
@@ -0,0 +1,147 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Windows.Forms;
+
+
+namespace VRoidHairMerger
+{
+    public partial class MergeForm : Form
+    {
+        public MergeForm()
+        {
+            InitializeComponent();
+
+            BindPresets();
+        }
+
+        /// <summary>
+        /// Load up available presets to combo boxes
+        /// </summary>
+        private void BindPresets()
+        {
+            // Get the path to hair_presets. This might need a better approach
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\LocalLow\\pixiv\\VRoidStudio\\hair_presets";
+
+            if(!Directory.Exists(path))
+            {
+                MessageBox.Show("Couldn't find the hair presets folder:\r\n" + path);
+                Application.Exit();
+            }
+
+            string[] presets = Directory.GetDirectories(path);
+            foreach(string pres in presets)
+            {
+                string p = new DirectoryInfo(pres).Name;
+
+                sourcePreset.Items.Add(p);
+                destPreset.Items.Add(p);
+            }
+
+
+        }
+
+        /// <summary>
+        /// Merge-button click
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Make sure we have presets selected
+            if (!String.IsNullOrEmpty(sourcePreset.Text) && !String.IsNullOrEmpty(destPreset.Text))
+            {
+                if (String.Compare(sourcePreset.Text, destPreset.Text) == 0)
+                {
+                    MessageBox.Show("Source and Destination must be different");
+                }
+                else
+                {
+                    // Get the preset paths
+                    String presetPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\LocalLow\\pixiv\\VRoidStudio\\hair_presets";
+
+                    string sourcePath = presetPath + "\\" + sourcePreset.Text;
+                    string destPath = presetPath + "\\" + destPreset.Text;
+
+                    // Make sure preset json-files exist
+                    if(File.Exists(sourcePath + "\\preset.json") &&
+                        File.Exists(destPath + "\\preset.json"))
+                    {
+                        // Lets read the preset files
+                       // MessageBox.Show("If VRoid shows an error when starting the program after this then make sure to go manually delete the destination preset folder:\r\n\r\n" + destPath, "Reminder");
+
+                        // Deserialize the preset json to dynamic
+                        dynamic sourceData = JObject.Parse(File.ReadAllText(sourcePath + "\\preset.json", Encoding.UTF8));
+                        dynamic destData = JObject.Parse(File.ReadAllText(destPath + "\\preset.json", Encoding.UTF8));
+
+                        int c = destData.Hairishes.Count;
+
+                        List<string> materialGuids = new List<string>();
+
+                        // Loop through the hair array
+                        for (int i=0;i< sourceData.Hairishes.Count;i++)
+                        {
+                            
+
+                            // Skip base hair since multiple instances will crash VRoid
+                            if(Convert.ToInt32(sourceData.Hairishes[i].Type ?? "0") != 3 )
+                            {
+                                // Copy hair data
+                                destData.Hairishes.Add(sourceData.Hairishes[i]);
+
+                                string materialID = sourceData.Hairishes[i].Param._MaterialValueGUID;
+                                if (!materialGuids.Contains(materialID))
+                                    materialGuids.Add(materialID);
+                                string materialInheritID = sourceData.Hairishes[i].Param._MaterialInheritedValueGUID;
+                                if(materialInheritID.Length > 0)
+                                {
+                                    if (!materialGuids.Contains(materialInheritID))
+                                        materialGuids.Add(materialInheritID);
+                                }
+
+
+                            }
+                        }
+                        //MessageBox.Show("Material count: " + materialGuids.Count);
+
+                        // Loop through materials
+                        for (int i = 0; i < sourceData._MaterialSet._Materials.Count; i++)
+                        {
+                            if(materialGuids.Contains((string)sourceData._MaterialSet._Materials[i]._Id))
+                            {
+                                destData._MaterialSet._Materials.Add(sourceData._MaterialSet._Materials[i]);
+                                // Copy material texture as well
+                                if(!File.Exists(destPath + "\\materials\\rendered_textures\\" + sourceData._MaterialSet._Materials[i]._MainTextureId + ".png"))
+                                    File.Copy(sourcePath + "\\materials\\rendered_textures\\" + sourceData._MaterialSet._Materials[i]._MainTextureId + ".png",
+                                        destPath + "\\materials\\rendered_textures\\" + sourceData._MaterialSet._Materials[i]._MainTextureId + ".png");
+                            }
+                        }
+
+
+                        // Loop through bones
+
+
+
+                        string json = Newtonsoft.Json.JsonConvert.SerializeObject(destData);
+
+                        Encoding utf8WithoutBom = new UTF8Encoding(false);
+
+                        File.WriteAllText(destPath + "\\preset2.json", json, utf8WithoutBom);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Either preset-file doesn't exist");
+                    }
+
+                }
+            }
+        }
+    }
+}
